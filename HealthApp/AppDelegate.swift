b005450d3ccdb6b1b47d7b7d7d4520d7eaf83824
TiparpronS. <96738451+SukanyaT2022@users.noreply.github.com>
2026-01-07 10:54:38 -0500

    remove 000

--- HealthApp/AppDelegate.swift
+++ HealthApp/AppDelegate.swift
@@ -61,7 +61,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return container
     }()
 
-    // MARK: - Core Data Saving support oooooo
+    // MARK: - Core Data Saving support 
 
     func saveContext () {
         let context = persistentContainer.viewContext
